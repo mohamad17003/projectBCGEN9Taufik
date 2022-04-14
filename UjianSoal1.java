dc7cff564dbea99c1bfbdde93d6610a2911e00c9
mohamad17003 <103627678+mohamad17003@users.noreply.github.com>
2022-04-14 20:54:01 +0700

    upload variasi cara soal 1 pengembangan
    ini adalah variasi cara mengerjakan soal 1

--- UjianSoal1.java
+++ UjianSoal1.java
@@ -16,7 +16,8 @@ public class UjianSoal1 {
 				if(i==0) {
 					array[i][j]=4*j+1;//1 5 9 13 17
 				}else {
-					array[i][j]=4*n-3-4*j;//array[0][n-1-j];//4*n-1-4*j;
+					//array[i][j]=4*n-3-4*j;//cara 2
+					array[i][j]=array[0][n-1-j];//cara 1
 				}
 			}
 		}
